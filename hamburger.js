0f06d8fc5b9c96bb1416f4f878cd918d292682a9
slennbom <sofia.lennbom@enghouse.com>
2025-02-18 14:32:26 +0100

    Fixed font size for all screens and created a .js file. Created html for hamburger.

---
+++ hamburger.js
@@ -0,0 +1,14 @@
+const hamburgerMenuClosed = document.getElementById("hamburger-menu-closed")
+
+const hamburgerMenuOpen = document.getElementById("hamburger-menu-open")
+
+// console.log(hamburgerMenuClosed)
+
+// hamburgerMenuClosed.onclick = () => {
+//   hamburgerMenuClosed.classList.toggle('hamburger-menu-open')
+//   hamburgerMenuOpen.display = "none"
+// }
+
+// hamburgerMenuClosed.onclick = () => {
+//   hamburgerMenuClosed.classList.toggle('hamburger-menu-open')
+// }
